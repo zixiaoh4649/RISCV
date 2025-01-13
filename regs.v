bf7f18445fb452c2f0a74aff2b82faab87a5c15d
Alan <zixiaoh3@illinois.edu>
2025-01-13 15:30:01 -0600

    complete regs

--- regs.v
+++ regs.v
@@ -1,16 +1,34 @@
-module pc_reg (
+module regs(
 	input wire clk,
 	input wire rst,
-	output reg [31:0] pc_o
+	input wire [4:0] rs1_addr,
+	input wire [4:0] rs2_addr,
+	output reg [31:0] rs1_data,
+	output reg [31:0] rs2_data
+	
 );
 
-	always @(posedge clk)begin
-
-		if (rst==1'b0)begin
-			pc_o<=32'b0;
-		end else begin
-			pc_o<=pc_o + 3'd4;
-		end
-	end
+	reg [31:0] regs [0:31];
 	
+	always @(*) begin
+		if(rst==1'b0) begin
+			rs1_data<=32'b0;		
+		end else if(rs1_addr==5'b0) begin
+			rs1_data<=32'b0;	
+		end else begin
+			rs1_data<=regs[rs1_addr];
+		end	
+	end 
+
+	always @(*) begin
+		if(rst==1'b0) begin
+			rs2_data<=32'b0;		
+		end else if(rs1_addr==5'b0) begin
+			rs2_data<=32'b0;	
+		end else begin
+			rs2_data<=regs[rs2_addr];
+		end	
+	end 
+
+
 endmodule
